1fa5ddff85c208f0c07ec1ce6b65076c8809e3e5
yovitaa <yovitaananda89@gmail.com>
2022-07-23 03:14:17 +0700

    initial commit

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -2,40 +2,40 @@
 // @route   GET /api/goals
 // @acccess Private
 
-const getGoals = (req,res) => {
+const getGoals = asyncHandler (async (req,res) => {
    
     
     res.status(200).json({message: "Get Goals"})
-}
+})
 
 // @desc    Set goals
 // @route   POST /api/goals
 // @acccess Private
 
-const setGoals = (req,res) => {
+const setGoals = asyncHandler (async  (req,res) => {
     if(!req.body.text) {
         res.status(400)
         throw new Error('Please add a text field')
     }
 
     res.status(200).json({message: 'Set Goals'})
-}
+})
 
 // @desc    Update goals
 // @route   PUT /api/goals
 // @acccess Private
 
-const updateGoals = (req,res) => {
+const updateGoals = asyncHandler (async (req,res) => {
     res.status(200).json({message: 'Update Goal ${req.params.id}'})
-}
+})
 
 // @desc    Delete goals
 // @route   DELETE /api/goals
 // @acccess Private
 
-const deleteGoals = (req,res) => {
+const deleteGoals = asyncHandler (async (req,res) => {
     res.status(200).json({message: 'Delete Goal ${req.params.id}'})
-}
+})
 
 module.exports = {
     getGoals,
