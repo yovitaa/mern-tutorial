2542ea8badaac06d85e27e7fdbb7354d755957c3
yovitaa <yovitaananda89@gmail.com>
2022-07-23 02:14:06 +0700

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
-const port = 5000
+const port = process.env.PORT || 5000
 
 const app = express()
 
+app.get('/api/goals',(req,res)=>{
+    res.status(200).json({message: "Get Goals"})
+   })
+   
 app.listen(port, () => console.log('Server started on port ${port}'))
