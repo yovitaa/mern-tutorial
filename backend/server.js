6315ad1b80b87903bae7543b80c77b09aa2fcd6f
yovitaa <yovitaananda89@gmail.com>
2022-07-23 02:17:50 +0700

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -7,5 +7,7 @@ const app = express()
 app.get('/api/goals',(req,res)=>{
     res.status(200).json({message: "Get Goals"})
    })
-   
+
+app.use('/api/goals',require('./routes/goalRoutes.js'))
+ 
 app.listen(port, () => console.log('Server started on port ${port}'))
