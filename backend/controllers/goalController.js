f97722e495d75fedc21a55218b0f731fc74f986b
yovitaa <yovitaananda89@gmail.com>
2022-07-23 02:40:09 +0700

    initial commit

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,7 +1,38 @@
+// @desc    Get goals
+// @route   GET /api/goals
+// @acccess Private
+
 const getGoals = (req,res) => {
-    res.status(200).json({message: 'Get Goals'})
-   }
-   module.exports = {
-    getGoals
-   }
-   
+    res.status(200).json({message: "Get Goals"})
+}
+
+// @desc    Set goals
+// @route   POST /api/goals
+// @acccess Private
+
+const setGoals = (req,res) => {
+    res.status(200).json({message: 'Set Goals'})
+}
+
+// @desc    Update goals
+// @route   PUT /api/goals
+// @acccess Private
+
+const updateGoals = (req,res) => {
+    res.status(200).json({message: 'Update Goal ${req.params.id}'})
+}
+
+// @desc    Delete goals
+// @route   DELETE /api/goals
+// @acccess Private
+
+const deleteGoals = (req,res) => {
+    res.status(200).json({message: 'Delete Goal ${req.params.id}'})
+}
+
+module.exports = {
+    getGoals,
+    setGoal,
+    updateGoal,
+    deleteGoal
+}
